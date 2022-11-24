2b0c7d67c3b3b8b310c47aacf9fc3f21e6d41b86
Damijan Cavar <damijan.cavar@turbinekreuzberg.com>
2022-11-24 13:55:34 +0100

    updated composer to latest version, fixed the yarn install issue ...

--- provision/docker-up.sh
+++ provision/docker-up.sh
@@ -2,7 +2,7 @@
 
 apt update
 if ! hash jq 2>/dev/null; then
-  apt install -y jq vim curl wget
+  apt install -y jq vim curl wget zip
 fi
 
 #install php
@@ -11,7 +11,6 @@ echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt
 wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
 
 apt update
-apt install -y zip
 apt install -y php8.1 \
 php8.1-curl \
 php8.1-bcmath \
@@ -20,22 +19,27 @@ php8.1-mbstring \
 php8.1-xml \
 php8.1-zip
 
-# install composer
-wget -q -O /tmp/composer-setup.php https://getcomposer.org/installer
-php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-rm /tmp/composer-setup.php
-
 # install nvm and node
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install node --lts
 npm install --global yarn
-cd /app
 yarn global add typescript
-yarn add jest
-yarn add --dev ts-jest
-yarn add --dev ts-node
-yarn add --dev @types/jest
+
+# setting up paths
+nvmYarn=$(which yarn)
+ln -s "$nvmYarn" /usr/local/bin/yarn
+
+nvmNode=$(which node)
+ln -s "$nvmNode" /usr/local/bin/node
+
+
+# install composer
+wget -q -O /tmp/composer-setup.php https://getcomposer.org/installer
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm /tmp/composer-setup.php
+
+
 
 
